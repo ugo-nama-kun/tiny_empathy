56e781b88ab5a90af9617ddbf5013125e5724e64
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2025-04-30 22:41:39 +0900

    remove wrapper for gird rooms

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="tiny_empathy_env",
-    version="1.8.3",
+    version="1.8.4",
     install_requires=["gymnasium==0.28.1", "pygame", "pettingzoo==1.24.3"]
 )
